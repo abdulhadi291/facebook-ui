fd2c1d5c51d543384225972f4e9b4955917a8bf1
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-07-08 13:45:39 +0500

    Facebook Post View

--- lib/post_view.dart
+++ lib/post_view.dart
@@ -11,20 +11,26 @@ class _PostViewState extends State<PostView> {
   TextEditingController postContentController = TextEditingController();
   List posts = [];
 
+  List<String> postImages = [
+    'https://upload.wikimedia.org/wikipedia/commons/7/70/Neeulm_Valley_AJK_%28Arang_Kel%29.jpg',
+    'https://i0.wp.com/stampedmoments.com/wp-content/uploads/2023/08/sonmarg-kashmir-2.jpg?fit=1024%2C576&ssl=1',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1QOKzEBqT5eyMm2vaCI-UlTi3NsVWLM8v6w&s',
+    'https://images.travelandleisureasia.com/wp-content/uploads/sites/3/2024/04/18152123/srinagar-1.jpeg?tr=w-480,f-jpg,pr-true'
+  ];
+
+  var imageIndex = 0;
+
   addPost() {
     posts.add({
       'content': postContentController.text,
-      'images': [
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-        'https://cdn.pixabay.com/photo/2016/03/11/02/08/speedometer-1249610_1280.jpg',
-      ]
+      'images': [postImages[imageIndex]]
     });
+
+    imageIndex++;
+    if (imageIndex >= postImages.length) {
+      imageIndex = 0;
+    }
+
     postContentController.clear();
     setState(() {});
     print(posts);
@@ -34,46 +40,113 @@ class _PostViewState extends State<PostView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Post Screen"),
+        backgroundColor: Color(0xFF1877F2),
+        title: Text(
+          "Facebook",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            letterSpacing: -1,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.message_outlined),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           TextField(
             controller: postContentController,
+            decoration: InputDecoration(
+              hintText: 'Whats on your mind..',
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ClipOval(
+                  child: Image.network(
+                    'https://cdn-icons-png.flaticon.com/512/6858/6858504.png',
+                    width: 10,
+                    height: 10,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              suffixIcon: IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  addPost();
+                },
+              ),
+            ),
           ),
           SizedBox(
             height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              addPost();
-            },
-            child: Text("Add item"),
           ),
           Expanded(
               child: ListView.builder(
             itemCount: posts.length,
             itemBuilder: (context, index) {
-              return Container(
-                child: Column(
-                  children: [
-                    Text(
+              return Column(
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
                       "${posts[index]['content']}",
-                    ),
-                    Image.network(
-                      posts[index]['images'][0],
-                    ),
-                    Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(posts[index]['images'][0]),
-                        ),
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ],
-                ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(posts[index]['images'][0]),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.thumb_up_alt_outlined,
+                            color: Colors.grey),
+                        label:
+                            Text("Like", style: TextStyle(color: Colors.grey)),
+                      ),
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.comment_outlined, color: Colors.grey),
+                        label: Text("Comment",
+                            style: TextStyle(color: Colors.grey)),
+                      ),
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.share_outlined, color: Colors.grey),
+                        label:
+                            Text("Share", style: TextStyle(color: Colors.grey)),
+                      ),
+                    ],
+                  )
+                ],
               );
             },
           ))
